988c990805671bbd7fbec65c24bc53fdbab355a4
Travis Nyland <122824127+HelloPingPong@users.noreply.github.com>
2025-05-30 16:18:41 -0400

    Create ReactDemoApplication.java

---
+++ react-demo-rest/src/main/java/com.jpmorgan.reactdemo/ReactDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jpmorgan.reactdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactDemoApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ReactDemoApplication.class, args);
+    }
+
+}
