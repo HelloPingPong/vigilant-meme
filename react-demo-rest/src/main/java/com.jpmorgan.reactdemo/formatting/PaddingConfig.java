80cfaf39ddae4126f779140a4b94ad5ef7f1c8fd
Travis Nyland <122824127+HelloPingPong@users.noreply.github.com>
2025-06-04 17:25:51 -0400

    Update PaddingConfig.java

--- react-demo-rest/src/main/java/com.jpmorgan.reactdemo/formatting/PaddingConfig.java
+++ react-demo-rest/src/main/java/com.jpmorgan.reactdemo/formatting/PaddingConfig.java
@@ -1,4 +1,10 @@
 package com.jpmorgan.reactdemo.formatting;
 
+import com.jpmorgan.reactdemo.formatting.enums.PaddingPosition;
+import lombok.Data;
+
+@Data
 public class PaddingConfig {
+    private Character character = ' '; // Default padding character
+    private PaddingPosition position = PaddingPosition.RIGHT; // Default position
 }
