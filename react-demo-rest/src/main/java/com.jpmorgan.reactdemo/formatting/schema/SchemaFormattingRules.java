d74e1b0bd9138026282694196360cdcac94427f1
Travis Nyland <122824127+HelloPingPong@users.noreply.github.com>
2025-06-04 17:04:13 -0400

    Update SchemaFormattingRules.java

--- react-demo-rest/src/main/java/com.jpmorgan.reactdemo/formatting/schema/SchemaFormattingRules.java
+++ react-demo-rest/src/main/java/com.jpmorgan.reactdemo/formatting/schema/SchemaFormattingRules.java
@@ -1,9 +1,7 @@
 package com.jpmorgan.reactdemo.formatting.schema;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,13 +11,13 @@ import java.util.Map;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SchemaFormattingRules {
     private GlobalFormattingRule globalRules;
-    private Map<String, CategoryFormattingRule> categoryRules; // e.g., "Name", "Address"
-    private Map<String, TypeFormattingRule> typeRules; // e.g., "Name.firstName"
+    private Map<String, CategoryFormattingRule> categoryRules;
+    private Map<String, TypeFormattingRule> typeRules;
     private List<ConditionalFormattingRule> conditionalRules;
 
     public SchemaFormattingRules() {
         this.categoryRules = new HashMap<>();
         this.typeRules = new HashMap<>();
-        this.conditionalRules = new List<ConditionalFormattingRule>();
+        this.conditionalRules = new ArrayList<>();
     }
 }
