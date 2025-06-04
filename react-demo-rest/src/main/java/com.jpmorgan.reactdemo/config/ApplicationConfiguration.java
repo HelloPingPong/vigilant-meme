d2bb010046b8c4bad88a9f093e98da970d506437
Travis Nyland <122824127+HelloPingPong@users.noreply.github.com>
2025-06-04 16:18:46 -0400

    Update ApplicationConfiguration.java

--- react-demo-rest/src/main/java/com.jpmorgan.reactdemo/config/ApplicationConfiguration.java
+++ react-demo-rest/src/main/java/com.jpmorgan.reactdemo/config/ApplicationConfiguration.java
@@ -1,20 +1,16 @@
 package com.jpmorgan.reactdemo.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.annotation.Configuration;
-import com.jpmorgan.moneta.boot.web.BrowserHistoryIndexResolver;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.jpmorgan.moneta.boot.web.BrowserHistoryIndexResolver;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApplicationConfiguration {
-
-    /**
-     * Redirects every page to index.html
-     */
 
     @Bean
     public ObjectMapper objectMapper() {
@@ -31,14 +27,19 @@ public class ApplicationConfiguration {
         return new XmlMapper();
     }
 
+    /**
+     * Browser history configuration for SPA support
+     */
     @Configuration
     public static class BrowserHistoryConfigurer implements WebMvcConfigurer {
 
         @Override
         public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-            registry.addResourceHandler("/**").addResourceLocations("classpath:/static/").setCachePeriod(3600)
-                    .resourceChain(true).addResolver(BrowserHistoryIndexResolver.builder().build());
+            registry.addResourceHandler("/**")
+                    .addResourceLocations("classpath:/static/")
+                    .setCachePeriod(3600)
+                    .resourceChain(true)
+                    .addResolver(BrowserHistoryIndexResolver.builder().build());
         }
-
     }
 }
